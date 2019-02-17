8656b5eecb532e3520acbd328e45f35a2e4c6d6b
Siddig MOhamed <siddigmohamed113@gmail.com>
2019-02-17 15:38:28 -0500

    Siddig commit

--- Generic/src/main/java/Base.java
+++ Generic/src/main/java/Base.java
@@ -1,19 +1,27 @@
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
+import org.apache.poi.hssf.util.CellReference;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import java.io.FileInputStream;
 
 public class Base {
     public WebDriver driver = null;
@@ -23,13 +31,14 @@ public class Base {
     @BeforeMethod
     public void setUp(boolean useSauceLab, String userName, String key, String appUrl, String os, String browserName, String browserVersion) throws IOException {
         if (useSauceLab == true) {
-            //getSauceLabDriver(userName, key, os, browserName, browserVersion);
+            getSauceLabDriver(userName, key, os, browserName, browserVersion);
         } else {
             getLocalDriver(os, browserName);
         }
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.navigate().to(appUrl);
         driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
         log.info("browser loaded with App");
     }
 
@@ -63,10 +72,49 @@ public class Base {
         return driver;
 
     }
+    //get cloud driver
+    public WebDriver getSauceLabDriver(String userName, String key, String os, String browserName,
+                                       String browserVersion )throws IOException{
+
+       DesiredCapabilities cap = new DesiredCapabilities();
+        cap.setCapability("platform", os);
+        cap.setBrowserName(browserName);
+        cap.setCapability("version", browserVersion);
+
+        driver = new RemoteWebDriver(new URL("http://"+ userName + ":" +  key +
+                "@ondemand.saucelabs.com:80/wd/hub"), cap);
+        return driver;
+    }
+
+    public void clickByXPath(String xpath) throws InterruptedException {
+        Thread.sleep(3000);
+        driver.findElement(By.xpath(xpath)).click();
+    }
+
+    public void sendTextByXpath(String xpath, String data) throws InterruptedException {
+        Thread.sleep(3000);
+        driver.findElement(By.xpath(xpath)).sendKeys(data);
+    }
+
+    public static String readFromExcel(String fileRef, String sheetRef, String cellRef) throws IOException {
+        FileInputStream fis = new FileInputStream(fileRef);
+        Workbook wb = new XSSFWorkbook(fis);
+        Sheet sheet = wb.getSheet(sheetRef);
+        DataFormatter formatter = new DataFormatter();
+        CellReference cellReference = new CellReference(cellRef);
+        Row row = sheet.getRow(cellReference.getRow());
+        Cell cell = row.getCell(cellReference.getCol());
+        String value = "";
+        if (cell != null) {
+            value = formatter.formatCellValue(cell);//cell.getStringCellValue();
+        }
+        return value;
+    }
 
     public void takeTheScreenshot(String testCaseName) throws Exception {
         Thread.sleep(3000);
-        String screenShotLocation = "C:\\Users\\sideeg\\Desktop\\Amazon\\Buncee\\src\\test\\TestResult\\Screenshot\\";
+        //String screenShotLocation = "C:\\Users\\sideeg\\Desktop\\Amazon\\Buncee\\src\\test\\TestResult\\Screenshot\\";
+        String screenShotLocation = "C:\\Users\\sideeg\\Desktop\\Craiglist\\Craiglist\\src\\test\\TestResult\\Screenshot";
         DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_HHmmss");
         Date date = new Date();
         String currentTime = dateFormat.format(date); //2016/11/16 12:08:43
